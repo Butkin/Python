caafe8ec9ba64a544120a93fbf12b9153b21593e
Butkin <99911082+Butkin@users.noreply.github.com>
2022-03-09 23:35:27 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+print("Hello world")
+
+age = 46
+print(age)
+
+price = 19.98
+first_name = "Piotr"
+is_online = False
+
+
+"/--------------------------------/"
+
+
+first = float(input("Podaj pierwsza wartość: "))
+second = float(input("Podaj drugą wartość: "))
+sum = first + second
+print ("Sum: " + str(sum))
